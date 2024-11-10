bbbc2bd9459079d53b718cbcb56bae8ef0065d37
Joakim Uddholm <tethik@blacknode.se>
2024-11-10 23:58:36 +0100

    adjust web app manifests

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,37 @@ import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      "name": "Mobile Starter",
+      "short_name": "Mobile Starter",
+      "lang": "en-US",
+      "icons": [
+        {
+          "src": "favicon.ico",
+          "sizes": "64x64 32x32 24x24 16x16",
+          "type": "image/x-icon"
+        },
+        {
+          "src": "logo192.png",
+          "type": "image/png",
+          "sizes": "192x192"
+        },
+        {
+          "src": "logo512.png",
+          "type": "image/png",
+          "sizes": "512x512"
+        }
+      ],
+      "start_url": ".",
+      "display": "standalone",
+      "theme_color": "#000000",
+      "background_color": "#ffffff",
+      "orientation": "portrait"
+    }
+  })
+  ],
   base: `/${pkg.name}/`,
 })
